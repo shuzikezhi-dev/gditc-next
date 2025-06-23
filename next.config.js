16300f1391093bfb8bb9f4300148d0df35182a44
gaoziyang <1052462170@qq.com>
2025-06-23 11:20:46 +0800

    Fix Cloudflare deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
+  distDir: 'out',
   images: {
     unoptimized: true,
     domains: ['wonderful-serenity-47deffe3a2.strapiapp.com']
   },
   env: {
     NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
+  },
+  experimental: {
+    images: {
+      unoptimized: true
+    }
   }
 }
 
